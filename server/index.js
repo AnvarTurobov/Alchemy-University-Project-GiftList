2f31581ddce35e2ef05626388a735a4defe27995
Anvar Turobov <anvar.turobov@gmail.com>
2024-05-09 17:24:06 +0100

    clean up

--- server/index.js
+++ server/index.js
@@ -7,23 +7,15 @@ const port = 1225;
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
-
 const niceList = require ('../utils/niceList.json');
 const merkleTree = new MerkleTree(niceList);
 const merkleRoot = merkleTree.getRoot();
-console.log( 'the merkle root : ' + merkleRoot );
 
 const MERKLE_ROOT = 'ddd59a2ffccddd60ff47993312821cd57cf30f7f14fb82937ebe2c4dc78375aa';
 
 app.post('/gift', (req, res) => {
-  // grab the parameters from the front-end here
   const body = req.body;
-
-  // TODO: prove that a name is in the list 
   const isInTheList = verifyProof(body.proof, body.leaf, MERKLE_ROOT);
-
 
   if(isInTheList) {
     res.send("You got a toy robot!");
